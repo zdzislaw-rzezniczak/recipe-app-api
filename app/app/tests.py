010d5f35309e39ce858bcd40eb855363a88b1b1e
Zdzislaw <zdzichrz@gmail.com>
2023-12-19 12:11:10 +0100

    psycopg2

---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+        self.assertEquals(res, 11)
+        
+    
+    def test_subtract_numbers(self):
+        res = calc.subtract(10,15)
+        self.assertEqual(res, -5)
