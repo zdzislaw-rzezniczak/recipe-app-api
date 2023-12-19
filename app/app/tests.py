d6ae739fa9950d5615205028ab3970ca1d62ff0d
Zdzislaw <zdzichrz@gmail.com>
2023-12-19 12:23:07 +0100

    psycopg2 lint2

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,11 +6,11 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    
+
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEquals(res, 11)
-    
+
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
         self.assertEqual(res, -5)
